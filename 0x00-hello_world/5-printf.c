f1e047dd0022e33571547267f6955522a9d2012c
SageMarscoleee <marscoleemarscolee@gmail.com>
2022-10-13 05:40:00 -0700

    Completed 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.f>
 /**
- * main - Entry
- * Return: Always 0
+ * main - Prints a text
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	printf("with proper grammar, but the outcome is a piece of art,")
 	return (0);
 }
