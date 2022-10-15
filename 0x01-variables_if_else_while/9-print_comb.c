01eb61617565c62a1f59795ea8277c3e7efbe9b1
SageMarscoleee <marscoleemarscolee@gmail.com>
2022-10-14 20:24:17 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,16 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 100; n++)
-
-		putchar(n + '0');
-	if (n < 99 )
+	for (n = 0; n <= 9; n++)
 	{
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
 		putchar(',');
 		putchar(' ');
 	}
+
 	putchar('\n');
+
 	return (0);
 }
