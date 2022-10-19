a58b80da0bc06322a8757eb03398dc309da4f47f
SageMarscoleee <marscoleemarscolee@gmail.com>
2022-10-19 11:50:36 +0000

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,11 @@ int main(void)
 
 	i = 0;
 
-	while (1 < 10)
-	{
-		putchar(1);
-	}
+	/* while (i < 10)*/
+	/* {*/
+	/* putchar(i);*/
+	/* }*/
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
