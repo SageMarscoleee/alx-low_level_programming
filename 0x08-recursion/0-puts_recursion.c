506f0686192c8c213800ed12701706d20a65fab3
SageMarscoleee <marscoleemarscolee@gmail.com>
2022-11-02 11:36:05 +0000

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,20 @@
 
 /**
 * Write a function that prints a string
-* Return = 0
+* Return 0
 */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	int n = 0;
+
+	if (s[n] == '\0');
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
 	else
 	{
-		_putchar('\n');
+		_putchar(s[n]);
+		_puts_recursion(s + 1);
 	}
 }
