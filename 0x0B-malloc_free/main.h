6598d2e3a13d6680dba7a464253f045b7808b4a9
SageMarscoleee <marscoleemarscolee@gmail.com>
2022-11-08 00:09:44 +0000

    files

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,5 +8,6 @@ int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
+int _putchar(char c);
 
 #endif
