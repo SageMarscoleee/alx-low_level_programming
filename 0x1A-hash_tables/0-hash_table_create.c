852bb1102d129cce0e737d65548ccc01baea1a8e
SageMarscoleee <marscoleemarscolee@gmail.com>
2023-02-03 19:47:30 +0000

    tasks solutions

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -29,6 +29,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		hash_table_created->array[i] = NULL;
 	}
-
 	return (hash_table_created);
 }
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -49,6 +49,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->value = value_copy;
 	new->next = ht->array[index];
 	ht->array[index] = new;
-
 	return (1);
 }
